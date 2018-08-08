04adaeb24043830061d3b2b593a52b0ba8fbeadb
acschall <37029433+acschall@users.noreply.github.com>
2018-08-08 14:25:17 -0700

    Add files via upload

--- Model.py
+++ Model.py
@@ -4,12 +4,16 @@ from mesa.datacollection import DataCollector
 import numpy as np
 import random
 
+'''To Do: '''
+'''Add age variable to each agent (can use to gatekeep about reproduction and death)'''
+'''Add visual representation of landholdings'''
 
 class HouseholdAgent(Agent):
     """ An agent with a randomly drawn initial harvest."""
-    def __init__(self, unique_id, model, lineage):
+    def __init__(self, unique_id, model, lineage, generation):
         super().__init__(unique_id, model)
         self.lineage = lineage
+        self.generation = generation
         self.food = self.model.standard_deviation * np.random.randn(1) + self.model.mean
         if self.food > 75:
             self.fed = 1
@@ -33,15 +37,15 @@ class HouseholdAgent(Agent):
 
 
 def reproduce(parent_agent, model):
-    rep_prob = .45
+    rep_prob = .25
     if random.uniform(0, 1) < rep_prob:
         model.num_agents = model.num_agents + 1
-        a = HouseholdAgent(model.num_agents, model, str(parent_agent.unique_id))
+        a = HouseholdAgent(model.num_agents, model, str(parent_agent.unique_id), model.step_number)
         model.schedule.add(a)
 
 
 def die(agent, model):
-    death_prob = .45
+    death_prob = .05
     if random.uniform(0, 1) < death_prob:
         model.schedule.remove(agent)
 
@@ -58,17 +62,20 @@ class MoneyModel(Model):
         self.mean = mean
         self.num_agents = N
         self.schedule = RandomActivation(self)
+        self.step_number = 0
 
         # Create agents
         for i in range(self.num_agents):
-            a = HouseholdAgent(i, self, str(i))
+            a = HouseholdAgent(i, self, str(i), self.step_number)
             self.schedule.add(a)
 
         self.datacollector = DataCollector(
             model_reporters={"Percent Households Fed": count_fed},
-            agent_reporters={"ID": lambda a: a.unique_id, "Lineage": lambda a: a.lineage}
+            agent_reporters={"Lineage": lambda a: a.lineage,
+                             "Generation": lambda a: a.generation}
         )
 
     def step(self):
+        self.step_number = self.step_number + 1
         self.datacollector.collect(self)
         self.schedule.step()
--- ModelRun.py
+++ ModelRun.py
@@ -1,6 +1,5 @@
 from Model import *
 import matplotlib.pyplot as plt
-import pandas as pd
 import networkx as nx
 
 # Give the model the number of agents you want
@@ -9,7 +8,7 @@ import networkx as nx
 model = MoneyModel(100, 30, 90)
 
 # Specify how many steps you want
-steps = 3
+steps = 10
 
 
 for i in range(steps):
@@ -17,70 +16,35 @@ for i in range(steps):
     print(model.schedule.get_agent_count())
     model.step()
 
-
 # Plot the number of households fed
 # num_fed = model.datacollector.get_model_vars_dataframe()
 # num_fed.plot()
 
 list_of_agents = model.datacollector.get_agent_vars_dataframe()
-# with pd.option_context('display.max_rows', None, 'display.max_columns', 3):
-    # print(list_of_agents)
-    # print(list_of_agents.xs(0, level="Step")["Lineage"])
-    # print(list_of_agents.xs((0, 1), level=['Step', 'Step'])["Lineage"])
-    # print(list_of_agents(12, level="AgentID")["Lineage"])
-
-
-''' NEED TO ADD CODE THAT SORTS OUT WHICH AGENTS ARE IN STEP ONE BUT NOT STEP 0'''
 
 
 def create_edges(graph):
     for i in range(1, steps):
-        for agent in list_of_agents.xs(i, level="Step")["ID"].values:
+        for agent in list_of_agents.xs(i, level="Step").index.values:
             agent_info = list_of_agents.xs((i, agent), level=["Step", "AgentID"])
             lineage = agent_info["Lineage"].values[0]
-            ID = agent_info["ID"].values[0]
-            graph.add_edge(int(ID), int(lineage))
+            graph.add_edge(int(lineage), agent)
 
 
+# Plot the family trees
+G = nx.DiGraph()
+for i in range(steps):
+    G.add_nodes_from(list_of_agents.xs(i, level="Step")[list_of_agents.xs(i, level="Step")["Generation"] == i].index.values,
+                     generation=i)
 
-
-# Add the relationships to the graph
-# for i in range(len(list_of_agents.index)):
-#     for j in range(steps):
-#         if i in list_of_agents.xs(j, level="Step")["ID"]:
-#             agent_info = list_of_agents.xs((j, i), level=["Step", "AgentID"])
-#             lineage = agent_info["Lineage"].values[0]
-#             ID = agent_info["ID"].values[0]
-#             print(lineage)
-#             print(ID)
-            # G.add_edge(ID, lineage)
-    # if i in list_of_agents.xs(0, level="Step")["ID"]:
-    #     agent_info = list_of_agents.xs((0, i), level=["Step", "AgentID"])
-    #     lineage = agent_info["Lineage"].values[0]
-    #     ID = agent_info["ID"].values[0]
-    #     G.add_node(ID)
-    # if i in list_of_agents.xs(1, level="Step")["ID"]:
-    #     agent_info = list_of_agents.xs((1, i), level=["Step", "AgentID"])
-    #     lineage = agent_info["Lineage"].values[0]
-    #     ID = agent_info["ID"].values[0]
-    #     G.add_node(ID)
-    #     G.add_edge(ID, lineage)
-
-G = nx.Graph()
-G.add_nodes_from(list_of_agents.xs(0, level="Step")["ID"].values)
-G.add_nodes_from(list_of_agents.xs(1, level="Step")["ID"].values)
-G.add_nodes_from(list_of_agents.xs(2, level="Step")["ID"].values)
 create_edges(G)
-
-
-# pos = nx.circular_layout(G, dim=2, scale=1)
-# pos = nx.drawing.nx_agraph.graphviz_layout(G, prog='dot')
-shells = [list_of_agents.xs(0, level="Step")["ID"].values,
-          list_of_agents.xs(1, level="Step")["ID"].values,
-          list_of_agents.xs(2, level="Step")["ID"].values]
-pos = nx.shell_layout(G, shells)
-plt.figure(figsize=(8, 8))
+values = [G.node[node]['generation'] for node in G.nodes]
+pos = nx.nx_pydot.graphviz_layout(G, prog='neato')
+plt.figure(figsize=(14, 14))
 plt.axis('equal')
-nx.draw(G, pos, node_size=20, alpha=0.5, node_color="blue", with_labels=False)
+nx.draw(G, pos, node_size=20, alpha=0.5, node_color=values, with_labels=False, cmap=plt.get_cmap('jet'))
+sm = plt.cm.ScalarMappable(cmap=plt.get_cmap('jet'), norm=plt.Normalize(vmin=0, vmax=steps))
+sm._A = []
+plt.colorbar(sm)
 
 plt.show()
